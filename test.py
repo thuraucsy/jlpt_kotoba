800498157d37a7f11c6506fd144481a9fd4bbea6
Thura Aung <thura.ucsy@gmail.com>
2020-03-19 22:25:10 +0900

    နံပါတ် 10 အထိပဲ build လုပ်ခိုင်းထားတာကိုဖျက်

--- test.py
+++ test.py
@@ -8,6 +8,6 @@ def saveToFile(row):
 with open('ttsMyanmar.csv') as csvfile:
 	reader = csv.reader(csvfile)
 	for index, row in enumerate(reader):
-		if index == 10: break
+		# if index == 10: break
 		print(','.join(row))
 		saveToFile(row)
