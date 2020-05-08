46d6be9ccbfedf73c11036bd7125515aecd48bc8
Thura Aung <thura.ucsy@gmail.com>
2020-05-08 20:02:34 +0900

    - tts save မလုပ်ခင် text ရှိမှ လုပ်ဖို့ ပြင်ဆင် - ဂျပန်စာမှာ（お）しょうがつ ဖြစ်ခဲ့ရင် （お）ကို split လုပ်လိုက်ပြီး ဘာတန်ဖိုးမှ မကျန်တော့လို့ တတ်လာတဲ့ error ကိုပြင်ဆင်

--- test.py
+++ test.py
@@ -7,9 +7,16 @@ def saveMM(row):
 	tts = gTTS(row[4], lang = 'my')
 	tts.save(f'build/tts/mm/{row[0]}.mp3')
 
+def splitUnwanted(jpWord):
+	if jpWord[0] == '（':
+		jpWord = jpWord.split('）')[1]
+	else:
+		jpWord = jpWord.split('（')[0]
+	return jpWord
+
 def saveJP(row):
-	kanji = row[3].split('（')[0]
-	kana = row[2].split('（')[0]
+	kanji = splitUnwanted(row[3])
+	kana = splitUnwanted(row[2])
 
 	pattern = re.compile("^([ァ-ンー]+)+$")
 	reResult = pattern.match(kana)
@@ -34,6 +41,6 @@ with open('Win Kotoba - TTS output.csv') as csvfile:
 	for index, row in enumerate(reader):
 		if index == 0: continue
 		print(f'{row[0]}, {row[4]}, {row[2]}, {row[3]}')
-		saveMM(row)
-		saveJP(row)
+		if row[4]: saveMM(row)
+		if row[2]: saveJP(row)
 		# if index == 10: break
